4d9606bb48499d47dd3b5bffc8062837ea6add56
1 <1@LAPTOP-2AF526SI>
2023-04-02 20:06:15 +0300

    Добавьте файлы проекта.

---
+++ PR12_2/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите текст");
+        string line = Console.ReadLine();
+        HashSet<char> setLine = TranslateLineToSet(line);
+        PrintSet(setLine);
+        HashSet<char> setDigit = new HashSet<char> { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        PrintSet(setDigit);
+        HashSet<char> newSet = setLine;
+        newSet.IntersectWith(setDigit);
+        PrintSet(newSet);
+        Console.ReadKey();
+    }
+    private static HashSet<char> TranslateLineToSet(string word)
+    {
+        HashSet<char> setWord = new HashSet<char>();
+        foreach (char c in word)
+        {
+            setWord.Add(c);
+        }
+        return setWord;
+    }
+    private static void PrintSet(HashSet<char> set)
+    {
+        foreach (char c in set)
+        {
+            Console.Write($"{c} ");
+        }
+        Console.WriteLine();
+    }
+}
